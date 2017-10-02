90af337ba812b90065ca0ce6dabcc49b4478e521
brianjgrier <brianjgrier@gmail.com>
2017-10-02 12:26:43 -0400

    Change to make more relevant
    Corrected the first line to allow running program directly.
    
    Also modified the second read function to showcase the use of  os.path.isfile and os.access

--- python_day1_func_try_except.py
+++ python_day1_func_try_except.py
@@ -1,10 +1,11 @@
-#/bin/python
+#!/usr/bin/python
 
+import os
 import sys, json, pprint
 
 def process_json_file(filename):
   try:
-    with open('.digest') as json_file:
+    with open(filename) as json_file:
       pp_descr = json.load(json_file)
       for keyval in pp_descr:
         print keyval, len(pp_descr[keyval])
@@ -16,28 +17,30 @@ def process_json_file(filename):
 
 
 def process_json_file_b(filename):
-  try:
-    pp_descr = None
-    file_p   = None
-    file_p   = open(filename)
+  path = './' + filename
+  pp_descr = None
+  file_p   = None
+  if os.path.isfile(path) and os.access(path, os.R_OK):
+#    print "File exists and is readable"
+    file_p   = open(path)
     pp_descr = json.load(file_p)
     for keyval in pp_descr:
       print keyval, len(pp_descr[keyval])
       if keyval == 'app' or keyval == 'alert' or keyval == 'event_policy':
         pprint.pprint(pp_descr[keyval])
     file_p.close()
-  except Exception as exc:
-    print 'function -', sys._getframe().f_code.co_name, 'Caught Exception:', exc
-
+  else:
+    print "Either file is missing or is not readable"
 
 
 process_json_file(filename='powerpack.json')
+raw_input('Please press "Enter"')
 process_json_file_b('powerpack.json')
-
+raw_input('Please press "Enter"')
 process_json_file('notArealFile')
+process_json_file_b('notArealFile')
 
 
 
 print 'Bye-Bye'
-
 
